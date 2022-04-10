3362c0e905255b3e52cafef7b7480a40c3ac0eda
Daniil <dtsikin@mail.ru>
2022-04-10 10:39:24 +0500

    Complete Task

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arr = {"123", "hi", "5290", "Daniil", "work", "-5"};
+
+void Check(string[] arr)
+{
+    int count = 0;
+    for(int i = 0; i<arr.Length; i++)
+    {
+        string number = arr[i];
+        if(number.Length<=3)
+        {
+            count++;
+        }
+    }
+
+    Console.WriteLine($"Количество строк с менее чем тремя символами: {count}");
+
+    string[] secondArr = new string[count];
+    Console.WriteLine("Новый массив: ");
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        string number = arr[i];
+
+        if(number.Length <= 3)
+        {
+            int j = 0;
+            secondArr[j] = number;
+            Console.WriteLine($"{secondArr[j]}");
+            j++;
+        }
+    }
+}
+
+Check(arr);
